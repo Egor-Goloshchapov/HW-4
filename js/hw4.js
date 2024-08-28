6ca699f24c68e5af9ea31db7798f6aca6201c375
Egor-Goloshchapov <141495264+Egor-Goloshchapov@users.noreply.github.com>
2024-08-28 15:12:34 +0300

    hw4

--- js/hw4.js
+++ js/hw4.js
@@ -4,23 +4,31 @@
 // }
 
 
-const price = prompt("Donate:");
-if(price > 10){
-    alert(`${price} Сума більша за 10`)
-} else{
-    alert(`${price} Сума менша або дорівнює 10`)
-}
+// const price = prompt("Donate:");
+// if(price > 10){
+//     alert(`${price} Сума більша за 10`)
+// } else{
+//     alert(`${price} Сума менша або дорівнює 10`)
+// }
 
-const js = "I love JavaScript";
-if(js.includes(`JavaScript`)){
-    alert(`Текст містить слово JavaScript`)
-} else{
-    alert(`Текст не містить слово JavaScript`)
-}
+// const js = "I love JavaScript";
+// if(js.includes(`JavaScript`)){
+//     alert(`Текст містить слово JavaScript`)
+// } else{
+//     alert(`Текст не містить слово JavaScript`)
+// }
 
-const number = prompt("Write a random number");
-if(10 <= number && number < 20){
-alert("Число входить в діапазон від 10 до 20")
-}else{
-    alert("Число не входить в діапазон від 10 до 20")
+// const number = prompt("Write a random number");
+// if(10 <= number && number < 20){
+// alert("Число входить в діапазон від 10 до 20")
+// }else{
+//     alert("Число не входить в діапазон від 10 до 20")
+// }
+
+
+const email = prompt("Write your E-mail:");
+if(email !== "."){
+    alert(`${email} you wrote not correct email`)
+} else{
+    alert(`${email} you wrote correct email`)
 }
